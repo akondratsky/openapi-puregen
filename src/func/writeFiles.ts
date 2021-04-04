d4adbfd1341bbf1d10deb62e430cc7fe7f476610
akondratsky <akondratsky@yandex.ru>
2021-04-04 21:52:04 +0300

    files are created even if folders not exist

--- src/func/writeFiles.ts
+++ src/func/writeFiles.ts
@@ -1,9 +1,10 @@
 import { promises as fs } from 'fs';
+import { dirname } from 'path';
 import { OutputData } from 'app/types';
-
 
 export const writeFiles = (data: OutputData[]): void => {
   data.forEach(({ content, output }) => {
+    fs.mkdir(dirname(output), { recursive: true });
     fs.writeFile(output, content)
       .then(() => {
         console.log(`File written: ${output}`);
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { loadConfiguration, loadSpecification } from './files';
 import { process as processData, checkPathsExist } from './func';
 import { argv } from './argv';
 
-const { spec, config, output } = argv;
+const { spec, config } = argv;
 
 if (!spec || !config) {
   console.error('Error: you should specify both config and specification files');
